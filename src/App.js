289b15547edb56b10366cc64241563ebb1f598b6
Vyacheslav B <la.sintez@gmail.com>
2017-08-12 14:07:26 +0300

    fix: moved IntlProvider into App component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import { Helmet } from "react-helmet";
 import Header from "./Header";
 import Product from "./Product";
 import Footer from "./Footer";
+import { IntlProvider, addLocaleData } from "react-intl";
+import ruLocaleData from "react-intl/locale-data/ru";
+
+addLocaleData(ruLocaleData);
 
 export default function App() {
   return (
-    <div>
-      <Helmet>
-        <title>Long Cotton Gabardine Car Coat | Men - Burberry</title>
-        <meta
-          name="description"
-          content="Invented by Thomas Burberry in 1879, cotton gabardine is a tightly woven and breathable fabric that protects against wind and rain."
-        />
-        <meta name="keywords" content="Gabardine Coat, Car Coat, Luxury" />
-      </Helmet>
-      <Header />
-      <Product />
-      <Footer />
-    </div>
+    <IntlProvider locale="ru">    
+      <div>
+        <Helmet>
+          <title>Long Cotton Gabardine Car Coat | Men - Burberry</title>
+          <meta
+            name="description"
+            content="Invented by Thomas Burberry in 1879, cotton gabardine is a tightly woven and breathable fabric that protects against wind and rain."
+          />
+          <meta name="keywords" content="Gabardine Coat, Car Coat, Luxury" />
+        </Helmet>
+        <Header />
+        <Product />
+        <Footer />
+      </div>
+    </IntlProvider>
   );
 }
