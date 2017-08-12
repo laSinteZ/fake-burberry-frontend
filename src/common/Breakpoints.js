716b214850f894df766ac5e014d680febc0625d1
Vyacheslav B <la.sintez@gmail.com>
2017-08-13 01:34:13 +0300

    fix: ok, breakpoint in rem now. but only LgOnly

--- src/common/Breakpoints.js
+++ src/common/Breakpoints.js
@@ -2,6 +2,6 @@ import React from "react";
 import Responsive from "react-responsive";
 
 export const LgOnly = ({ children }) =>
-  <Responsive minWidth={992} children={children} />;
+  <Responsive minWidth="62rem" children={children} />;
 export const Md = ({ children }) =>
   <Responsive maxWidth={991} children={children} />;
